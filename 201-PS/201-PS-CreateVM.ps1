1547a91a8859c1ba8fe313de69ececb8d4e6bbd2
Jürgen Lobenz <juergen.lobenz@microsoft.com>
2018-11-30 16:01:15 +0100

    PS Credentials and Continue

--- 201-PS/201-PS-CreateVM.ps1
+++ 201-PS/201-PS-CreateVM.ps1
@@ -5,16 +5,23 @@ Set-AzureRmContext -SubscriptionId "180b44f4-1d54-4817-87ef-22ca8f374006"
 # Create RG
 New-AzureRmResourceGroup -Name "201-Powershell-Demo-RG" -Location WestEurope
 
+# Generate credentials
+$secpasswd = ConvertTo-SecureString "Passw0rd!12345" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ("vmadmin", $secpasswd)
+
+# Create VM
 New-AzureRmVm `
     -ResourceGroupName "201-Powershell-Demo-RG" `
     -Name "Powershell-Demo" `
     -Location "West Europe" `
-    -Credential (Get-Credential) `
+    -Credential $mycreds `
     -VirtualNetworkName "Powershell-Demo-VNET" `
     -SubnetName "mySubnet" `
     -SecurityGroupName "Powershell-Demo-NSG" `
     -PublicIpAddressName "Powershell-Demo-IP" `
-    -OpenPorts 80,3389
+    -OpenPorts 3389
 
+Read-Host 'Press Enter to continue and clean up…' | Out-Null
 
+# Clean Up
 Remove-AzureRmResourceGroup -Name "201-Powershell-Demo-RG" -Force
