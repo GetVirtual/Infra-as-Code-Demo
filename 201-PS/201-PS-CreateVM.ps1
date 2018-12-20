1d562d15b67343748e4ebffe9884770bf28f2328
GetVirtual <42057794+GetVirtual@users.noreply.github.com>
2018-12-20 14:32:02 +0100

    Replace AzureRM

--- 201-PS/201-PS-CreateVM.ps1
+++ 201-PS/201-PS-CreateVM.ps1
@@ -1,27 +1,31 @@
-# Login to Azure (dont forget within VS Code Popup will be hidden in background ...)
-# Connect-AzureRmAccount
-Set-AzureRmContext -SubscriptionId "180b44f4-1d54-4817-87ef-22ca8f374006"
+# Vars
+$SubscriptionName = "Microsoft Azure Internal Consumption" #adjust to your Subscription Name
+$RGName = "201-Powershell-Demo-RG"
+$Location = "West Europe"
+$VMName = "Powershell-Demo"
+
+# Connect to Azure with Device Login
+Connect-AzAccount
+
+# Set Subscription Context
+Set-AzContext -SubscriptionName $SubscriptionName
 
 # Create RG
-New-AzureRmResourceGroup -Name "201-Powershell-Demo-RG" -Location WestEurope
+New-AzResourceGroup -name $RGName -location $Location
 
 # Generate credentials
 $secpasswd = ConvertTo-SecureString "Passw0rd!12345" -AsPlainText -Force
 $mycreds = New-Object System.Management.Automation.PSCredential ("vmadmin", $secpasswd)
 
 # Create VM
-New-AzureRmVm `
-    -ResourceGroupName "201-Powershell-Demo-RG" `
-    -Name "Powershell-Demo" `
-    -Location "West Europe" `
-    -Credential $mycreds `
-    -VirtualNetworkName "Powershell-Demo-VNET" `
-    -SubnetName "mySubnet" `
-    -SecurityGroupName "Powershell-Demo-NSG" `
-    -PublicIpAddressName "Powershell-Demo-IP" `
-    -OpenPorts 3389
+New-AzVM -Name $VMName -ResourceGroupName $RGName -Location $Location -Credential $mycreds `
+-VirtualNetworkName ("VNET-" + $VMName) -SubnetName ("VNET-" + $VMName) `
+-Size "Standard_B2ms" -Image "Win2019Datacenter" `
+-PublicIpAddressName ("PublicIP-" + $VMName) -SecurityGroupName ("NSG-" + $VMName) -OpenPorts "3389"
 
-Read-Host 'Press Enter to continue and clean up…' | Out-Null
+Write-Host "Connect with MSRDP to Public IP: " (Get-AzPublicIpAddress -ResourceGroupName $RGName -name ("PublicIP-" + $VMName)).IpAddress
 
-# Clean Up
-Remove-AzureRmResourceGroup -Name "201-Powershell-Demo-RG" -Force
+# https://www.youtube.com/watch?v=st6-DgWeuos
+Read-Host "To continue with VM Cleanup process press the any key... (where is the any key?!)"
+
+Remove-AzResourceGroup -Name $RGName -Force:$true
