22da652b809eea03e3f7cc0e28963f7a88729892
Jürgen Lobenz <juergen.lobenz@microsoft.com>
2019-05-20 16:09:34 +0200

    changed backend key

--- 301-Terraform/main.tf
+++ 301-Terraform/main.tf
@@ -16,7 +16,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "jloremotetfstate"
     container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "iacdemo.terraform.tfstate"
 
     # rather than defining this inline, the Access Key can also be sourced
     # from an Environment Variable - more information is available below.
